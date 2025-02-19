4100a4ca614af1252f8765bd755b3ae7b3dc9600
Nurlintang Asriono Sudarmawan <135283434+Nurlintang@users.noreply.github.com>
2025-02-19 15:13:27 +0700

    Update script.js

--- main_menu/script.js
+++ main_menu/script.js
@@ -14,7 +14,7 @@ updateGreeting();
 
 // Dummy Interest Rates
 const interestRates = [
-    { country: "USA", rate: "4.75%" },
+    { country: "USA", rate: "4.5%" },
     { country: "Eurozone", rate: "3.4%" },
     { country: "Japan", rate: "0.10%" },
 ];
